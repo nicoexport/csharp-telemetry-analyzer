a497d1e10c2538a5cec675f9b4050f3a3ac2427b
Nico Auerswald <auersw.nico@gmail.com>
2026-05-18 19:30:21 +0200

    add Paths helper in shared

--- src/TelemetryGenerator/Program.cs
+++ src/TelemetryGenerator/Program.cs
@@ -1,14 +1,21 @@
-﻿const int EVENT_COUNT = 500; 
+﻿using Telemetry.Shared;
+
+const int EVENT_COUNT = 500; 
 
 Console.WriteLine("Generating a dummy log file!");
 
+
+var path = Paths.GetTelemetryLogPath();
+
 TelemetryEventGenerator generator = new();
 
-using (var writer = new LogFileWriter("telemetry.log"))
+using (var writer = new LogFileWriter(path))
 {
     foreach (var e in generator.Generate(EVENT_COUNT))
     {
         writer.WriteLine(e);
     }
 }
+
+Console.WriteLine($"Log written to: {path}");
 
---
+++ src/TelemetryShared/Paths.cs
@@ -0,0 +1,23 @@
+namespace Telemetry.Shared;
+
+public static class Paths
+{
+    public static string GetOutputDirectory()
+    {
+        string solutionRoot = Path.GetFullPath(
+            Path.Combine(AppContext.BaseDirectory, "../../../../../"));
+
+        string outputDir = Path.Combine(solutionRoot, "output");
+
+        Directory.CreateDirectory(outputDir);
+
+        return outputDir;
+    }
+
+    public static string GetTelemetryLogPath()
+    {
+        return Path.Combine(
+            GetOutputDirectory(),
+            "telemetry.log");
+    }
+}
